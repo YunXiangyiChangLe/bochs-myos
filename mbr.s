bfd6d1bcad189738fd8b3a8ccc2b02a5ca6fef14
shaole <2728039823@qq.com>
2023-05-21 11:28:38 +0800

    修改mbr

--- mbr.s
+++ mbr.s
@@ -5,6 +5,8 @@ SECTION MBR vstart=0x7c00
     mov ss,ax
     mov fs,ax
     mov sp,0x7c00
+    mov ax,0xb800
+    mov gs,ax
 
 ;清屏
     mov ax,0x600
@@ -13,18 +15,36 @@ SECTION MBR vstart=0x7c00
     mov dx,0x184f
     int 0x10
 
-;获取光标位置
-    mov ah,3
-    mov bh,0
-    int 0x10
+; ;获取光标位置
+;     mov ah,3
+;     mov bh,0
+;     int 0x10
 
-;打印字符串
-    mov ax,message
-    mov bp,ax
-    mov cx,9
-    mov ax,0x1301
-    mov bx,0x2
-    int 0x10
+; ;打印字符串
+;     mov ax,message
+;     mov bp,ax
+;     mov cx,9
+;     mov ax,0x1301
+;     mov bx,0x2
+;     int 0x10
+
+
+    mov byte [gs:0x00],'1'
+    mov byte [gs:0x01],0xA4
+
+    mov byte [gs:0x02],' '
+    mov byte [gs:0x03],0xA4
+
+    mov byte [gs:0x04],'M'
+    mov byte [gs:0x05],0xA4
+
+    mov byte [gs:0x06],'B'
+    mov byte [gs:0x07],0xA4
+
+    mov byte [gs:0x08],'R'
+    mov byte [gs:0x09],0xA4
+
+
 
     jmp $
 
